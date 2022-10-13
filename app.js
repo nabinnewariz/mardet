7f29d0971c0b2a016dfe9a96ecbec6b5865fcbd0
n.newar123@gmail.com <n.newar123@gmail.com>
2022-10-13 11:06:17 +0530

    cors

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const cors = require('cors');
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
@@ -10,17 +9,23 @@ require('./database/connection');
 app.use(express.json());
 app.use(require('./router/register'));
 app.use(require('./router/guestDetails'));
-const helmet = require('helmet');
-app.use(helmet.referrerPolicy({policy: 'strict-origin-when-cross-origin'}));
+// const helmet = require('helmet');
+// app.use(helmet.referrerPolicy({policy: 'strict-origin-when-cross-origin'}));
 const PORT = process.env.PORT || 3000;
-app.use(cors());
+const cors = require('cors');
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT, PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}));
 const bodyParser = require('body-parser');
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
- });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//  });
 // app.options('*', cors());
 // app.use(
 //     cors({
